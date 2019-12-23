5f2479083993ff82fbe7e2eab5c08807a9e921d1
alex <alex@openpy.org>
2019-12-23 18:16:25 +0800

    更换主题

--- _travis.sh
+++ _travis.sh
@@ -1,7 +1,7 @@
 #--------------------------------------------
 #!/bin/bash
-# author：shenliyang
-# website：https://github.com/shenliyang
+# author：alex
+# website：https://github.com/desmond56
 # slogan：梦想还是要有的，万一实现了呢。
 #--------------------------------------------
 
@@ -58,8 +58,6 @@ EOF
   git tag v1.2.$TRAVIS_BUILD_NUMBER -a -m "Auto Taged By TravisCI With Build $TRAVIS_BUILD_NUMBER"
   # Github Pages
   git push --quiet "https://${GH_TOKEN}@${GH_REF}" master:master --tags
-  # Gitee Pages
-#  git push --quiet "https://shenliyang:${GITEE_TOKEN}@${GITEE_REF}" master:master --tags
 }
 
 case $1 in
