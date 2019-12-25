44213363a34f83772259ed0208dabb729288d81e
alex <alex@openpy.org>
2019-12-25 18:38:32 +0800

    优化

--- themes/snippet/scripts/process.js
+++ themes/snippet/scripts/process.js
@@ -1,5 +1,5 @@
 hexo.on('ready', function() {
-    hexo.log.info("=========================================");
-    hexo.log.info("  Welcome to use Snippet theme for hexo  ");
-    hexo.log.info("=========================================");
+    hexo.log.info("=============================================");
+    hexo.log.info("  Welcome to use Alex's Blog theme for hexo  ");
+    hexo.log.info("=============================================");
 });
